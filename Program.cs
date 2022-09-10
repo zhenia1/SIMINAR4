be337505238ef8ea07ca1f13d7f79e74053e5bdb
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-11 02:40:14 +0500

    TASK4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 25: Напишите цикл,который принимает на вход два числа (А и В) и возводит число А в натуральную степинь В.
+// 3, 5 -> 243  (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
+
+int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ: " + exponentiation);
